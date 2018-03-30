acd3beeaf6ff51acd2e33bf085f0416c67fc8cec
Roberto Ferraris <ro.ferraris@gmail.com>
2018-03-30 15:22:05 +0200

    Added service to HTTP

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product-service";
 
 @Component({
     selector: 'pm-products',
@@ -9,6 +10,7 @@ import { IProduct } from "./product";
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     showImage: boolean = false;
+    errorMessage: string;
     private _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
@@ -18,36 +20,21 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
-    constructor() {
-        this._listFilter = null;
-        this.filteredProducts = this.products;
+    products: IProduct[];
+    constructor(private _productService: ProductService) {
     }
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
     ngOnInit(): void {
+        this._productService.getProducts()
+            .subscribe(
+                data => {
+                        this.products = data;
+                        this.filteredProducts = this.products;
+                },
+                error => this.errorMessage = <any>error);
+        
         console.log('In OnInit');
     }
     private performFilter(filterBy: string): IProduct[] {
