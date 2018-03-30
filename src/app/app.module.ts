5d7971563bcaa0dd65e85559eb66bf9383e45770
Roberto Ferraris <ro.ferraris@gmail.com>
2018-03-30 19:48:53 +0200

    Inserted routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,31 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent, ConvertToSpaces, StarComponent
+    AppComponent, 
+    ProductListComponent, 
+    ConvertToSpaces, 
+    StarComponent, 
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule, 
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+      //{ path: '**', component: WelcomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+  constructor(private _route: ActivatedRoute) { 
+  }
+
+  ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    console.log(id)
+  }
+
+}
